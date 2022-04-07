598ac8d36869b022ed3533480ac0ab2cb4101eab
Emanuel Tesar <e.tesarr@gmail.com>
2022-04-07 11:23:31 +0200

    Add retry delay functionality

--- src/index.ts
+++ src/index.ts
@@ -3,9 +3,20 @@ export type GoResultSuccess<T> = { data: T; success: true };
 export type GoResultError<E extends Error = Error> = { error: E; success: false };
 export type GoResult<T, E extends Error = Error> = GoResultSuccess<T> | GoResultError<E>;
 
+export interface StaticDelayOptions {
+  type: 'static';
+  delayMs: number;
+}
+
+export interface RandomDelayOptions {
+  type: 'random';
+  minDelayMs: number;
+  maxDelayMs: number;
+}
 export interface GoAsyncOptions {
   readonly retries?: number;
   readonly timeoutMs?: number;
+  readonly delay?: StaticDelayOptions | RandomDelayOptions;
 }
 
 export class GoWrappedError extends Error {
@@ -53,17 +64,19 @@ export const goSync = <T, E extends Error>(fn: () => T): GoResult<T, E> => {
   }
 };
 
+const getRandomInRange = (min: number, max: number) => {
+  return Math.random() * (max - min) + min;
+};
+
+const sleep = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms));
+const timeout = (ms: number) => new Promise((_, reject) => setTimeout(() => reject('Operation timed out'), ms));
+
 const attempt = async <T, E extends Error>(fn: () => Promise<T>, timeoutMs?: number): Promise<GoResult<T, E>> => {
   // We need try/catch because `fn` might throw sync errors as well
   try {
     if (timeoutMs === undefined) return success(await fn());
     else {
-      return success(
-        await Promise.race([
-          fn(),
-          new Promise((_, reject) => setTimeout(() => reject('Operation timed out'), timeoutMs)) as Promise<T>,
-        ])
-      );
+      return success(await Promise.race([fn(), timeout(timeoutMs) as Promise<T>]));
     }
   } catch (err) {
     return createGoError(err);
@@ -77,7 +90,7 @@ export const go = async <T, E extends Error>(
   const fn = typeof predicate === 'function' ? predicate : () => predicate;
   if (!options) return attempt(fn);
 
-  const { retries, timeoutMs } = options;
+  const { retries, timeoutMs, delay } = options;
   const attempts = retries ? retries + 1 : 1;
   let lastFailedAttemptResult: GoResultError<E> | null = null;
   for (let i = 0; i < attempts; i++) {
@@ -85,6 +98,20 @@ export const go = async <T, E extends Error>(
     if (goRes.success) return goRes;
 
     lastFailedAttemptResult = goRes;
+    if (delay) {
+      switch (delay.type) {
+        case 'random': {
+          const { minDelayMs, maxDelayMs } = delay;
+          await sleep(getRandomInRange(minDelayMs, maxDelayMs));
+          break;
+        }
+        case 'static': {
+          const { delayMs } = delay;
+          await sleep(delayMs);
+          break;
+        }
+      }
+    }
   }
 
   return lastFailedAttemptResult!;
